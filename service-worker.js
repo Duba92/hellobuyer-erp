b58fbbcbfb4fbe9235e4febc98ddeda134bcf3d1
Duba Dida orobo <duba.519917@haramaya.edu.et>
2026-05-13 01:37:32 +0300

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,41 +1,34 @@
-const CACHE_NAME = 'hellobuyer-v1';
+const CACHE_NAME = "erp-cache-v1";
+
 const urlsToCache = [
-  './',
-  './index.html',
-  './products.html',
-  './pos.html',
-  './reports.html',
-  './css/style.css',
-  './js/app.js',
-  './js/products.js',
-  './js/pos.js',
-  './js/reports.js',
-  './js/offline.js',
-  './manifest.json'
+  "/",
+  "/index.html",
+  "/css/style.css",
+  "/js/app.js"
 ];
 
-self.addEventListener('install', event => {
+self.addEventListener("install", event => {
+
   event.waitUntil(
+
     caches.open(CACHE_NAME)
-      .then(cache => cache.addAll(urlsToCache))
-      .then(() => self.skipWaiting())
+      .then(cache => {
+        return cache.addAll(urlsToCache);
+      })
+
   );
 });
 
-self.addEventListener('fetch', event => {
+self.addEventListener("fetch", event => {
+
   event.respondWith(
-    caches.match(event.request)
-      .then(response => response || fetch(event.request))
-  );
-});
 
-self.addEventListener('activate', event => {
-  event.waitUntil(
-    caches.keys().then(keys => {
-      return Promise.all(
-        keys.filter(key => key !== CACHE_NAME)
-          .map(key => caches.delete(key))
-      );
-    })
+    caches.match(event.request)
+      .then(response => {
+
+        return response || fetch(event.request);
+
+      })
+
   );
 });
